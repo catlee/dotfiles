bc49eb35068a1a6c97cfab7177293a07cf250de8
Chris AtLee <chris.atlee@shopify.com>
2022-10-05 13:25:23 +0000

    clipboard support in spin

--- vim/user_init.lua
+++ vim/user_init.lua
@@ -11,5 +11,18 @@ return {
     treesitter = {
       endwise = { enable = true },
     },
+  },
+
+  polish = function()
+    if vim.env.SPIN == '1' then
+      -- Enable copy/paste on spin
+      vim.cmd([[
+      let g:clipboard = {
+        \ 'name': 'pbcopy',
+        \ 'copy': {'+': 'pbcopy', '*': 'pbcopy'},
+        \ 'paste': {'+': 'pbpaste', '*': 'pbpaste'},
+        \ 'cache_enabled': 1 }
+        ]])
+      end
+    end,
   }
-}
