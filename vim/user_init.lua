533e970cea4907c73290b1e8857adfed3e3f87db
Chris AtLee <chris.atlee@shopify.com>
2022-10-21 17:38:37 +0000

    vimtest

--- vim/user_init.lua
+++ vim/user_init.lua
@@ -19,12 +19,21 @@ return {
 
   mappings = {
     n = {
+      ["<leader>E"] = { "<cmd>e ~/.config/nvim/lua/user/init.lua<cr>", desc = "Edit init.lua" },
+
       ["<leader>TA"] = { "<cmd>TestSuite<cr>", desc = "Test Suite" },
       ["<leader>TT"] = { "<cmd>TestLast<cr>", desc = "Test Last" },
+      ["<leader>Tt"] = { "<cmd>TestNearest<cr>", desc = "Test Nearest" },
+      ["<leader>Tf"] = { "<cmd>TestFile<cr>", desc = "Test File" },
+    },
+    t = {
+      -- Ctrl-O in terminal to enter normal mode
+      ["<C-O>"] = { "<C-\\><C-n>" },
     }
   },
 
   polish = function()
+    vim.g['test#strategy'] = 'neovim'
     if vim.env.SPIN == '1' then
       -- Enable copy/paste on spin
       vim.cmd([[
