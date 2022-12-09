5c1772d1885d71dbe3e665a1523d0036e1d49cb0
Chris AtLee <chris.atlee@shopify.com>
2022-12-09 19:42:33 +0000

    more plugins

--- vim/user_init.lua
+++ vim/user_init.lua
@@ -14,6 +14,8 @@ return {
       { "justinmk/vim-sneak" },
       { "folke/tokyonight.nvim" },
       { "junegunn/vim-easy-align" },
+      { "michaeljsmith/vim-indent-object" },
+      { "nvim-treesitter/nvim-treesitter-textobjects" },
       (vim.env.SPIN == '1' and { "Shopify/spin-hud" }) or nil,
     },
     treesitter = {
@@ -22,6 +24,16 @@ return {
         "diff", "regex", "sql", "json", "json5", "lua"
       },
       endwise = { enable = true },
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+          }
+        }
+      }
     },
     ["mason-lspconfig"] = {
       ensure_installed = { "rust_analyzer" },
