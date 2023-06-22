2fd750b9fb7d8ba51370ebf2e3bbfa2af81af5d1
Chris AtLee <chris.atlee@shopify.com>
2023-06-22 17:09:27 +0000

    Get diagnostics working

--- vim/user_init.lua
+++ vim/user_init.lua
@@ -1,3 +1,4 @@
+local vim = vim;
 local function get_clipboard()
   if vim.env.SPIN == "1" then
     return {
@@ -220,15 +221,46 @@ return {
       timeout_ms = 10000
     },
     config = {
-      ruby_ls = function()
+      ruby_ls = function(opts)
         local util = require "lspconfig.util";
-        return {
-          cmd = { 'bundle', 'exec', 'ruby-lsp' },
-          root_dir = util.find_git_ancestor,
-          init_options = {
-            formatting = "auto"
-          }
-        }
+        opts.cmd = { 'bundle', 'exec', 'ruby-lsp' }
+        opts.root_dir = util.find_git_ancestor()
+        opts.init_options = { formatting = "auto" }
+        opts.on_attach = function(client, buffer)
+          -- in the case you have an existing `on_attach` function
+          -- with mappings you share with other lsp clients configs
+          pcall(on_attach, client, buffer)
+          local diagnostic_handler = function()
+            local params = vim.lsp.util.make_text_document_params(buffer)
+            client.request(
+              'textDocument/diagnostic',
+              { textDocument = params },
+              function(err, result)
+                if err then
+                  local err_msg = string.format("ruby-lsp - diagnostics error - %s", vim.inspect(err))
+                  vim.lsp.log.error(err_msg)
+                end
+                if not result then return end
+                vim.lsp.diagnostic.on_publish_diagnostics(
+                  nil,
+                  vim.tbl_extend('keep', params, { diagnostics = result.items }),
+                  { client_id = client.id }
+                )
+              end
+            )
+          end
+          diagnostic_handler() -- to request diagnostics when attaching the client to the buffer
+          local ruby_group = vim.api.nvim_create_augroup('ruby_ls', { clear = false })
+          vim.api.nvim_create_autocmd(
+            { 'BufEnter', 'BufWritePre', 'InsertLeave', 'TextChanged' },
+            {
+              buffer = buffer,
+              callback = diagnostic_handler,
+              group = ruby_group,
+            }
+          )
+        end
+        return opts
       end
     },
   },
