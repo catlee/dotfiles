ff20430ea71f269d85e252225c7dc3e20aad8055
Chris AtLee <chris.atlee@shopify.com>
2023-06-29 11:31:55 +0000

    Use stylua instead of lua_ls

--- vim/user_init.lua
+++ vim/user_init.lua
@@ -1,15 +1,15 @@
-local vim = vim;
+local vim = vim
 local function get_clipboard()
   if vim.env.SPIN == "1" then
     return {
-      name = 'pbcopy',
+      name = "pbcopy",
       copy = {
-        ['+'] = 'pbcopy',
-        ['*'] = 'pbcopy'
+        ["+"] = "pbcopy",
+        ["*"] = "pbcopy",
       },
       paste = {
-        ['+'] = 'pbpaste',
-        ['*'] = 'pbpaste'
+        ["+"] = "pbpaste",
+        ["*"] = "pbpaste",
       },
       cache_enabled = 1,
     }
@@ -24,56 +24,50 @@ return {
   },
   plugins = {
     -- Undo tree
-    { "mbbill/undotree",                keys = { { "<leader>u", vim.cmd.UndotreeToggle } } },
+    { "mbbill/undotree", keys = { { "<leader>u", vim.cmd.UndotreeToggle } } },
     -- Git integration
-    { "tpope/vim-fugitive",             cmd = { "G", "Git" } },
+    { "tpope/vim-fugitive", cmd = { "G", "Git" } },
     -- Adds `end` in Ruby, Lua, etc.
     { "RRethy/nvim-treesitter-endwise", lazy = false },
     -- Better % matching
-    { "andymass/vim-matchup",           lazy = false },
+    { "andymass/vim-matchup", lazy = false },
     -- Manage surrounding characters
-    { "tpope/vim-surround",             lazy = false },
+    { "tpope/vim-surround", lazy = false },
     -- Repeat stuff
-    { "tpope/vim-repeat",               lazy = false },
+    { "tpope/vim-repeat", lazy = false },
     -- Better targest, incl. q for quoted strings
-    { "wellle/targets.vim",             lazy = false },
+    { "wellle/targets.vim", lazy = false },
     -- Jump to text with s/S
     {
       "ggandor/leap.nvim",
       lazy = false,
-      init = function()
-        require("leap").add_default_mappings()
-      end,
+      init = function() require("leap").add_default_mappings() end,
     },
     -- Better f/t
     {
       "ggandor/flit.nvim",
       lazy = false,
-      init = function()
-        require("flit").setup()
-      end
+      init = function() require("flit").setup() end,
     },
     -- Use virtual lines to display diagnostic text
     {
       "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
       lazy = false,
-      init = function()
-        require("lsp_lines").setup()
-      end,
+      init = function() require("lsp_lines").setup() end,
     },
     -- Show all your problems
     {
       "folke/trouble.nvim",
       keys = {
         { "<leader>Tx", function() require("trouble").toggle() end, desc = "Show trouble" },
-      }
+      },
     },
     -- Theme
     { "dracula/vim" },
     -- Align text based on content
-    { "junegunn/vim-easy-align",                     keys = { { "ga", "<Plug>(EasyAlign)", mode = "x" } } },
+    { "junegunn/vim-easy-align", keys = { { "ga", "<Plug>(EasyAlign)", mode = "x" } } },
     -- Indentation text objects
-    { "michaeljsmith/vim-indent-object",             lazy = false },
+    { "michaeljsmith/vim-indent-object", lazy = false },
     -- Language aware text objects
     { "nvim-treesitter/nvim-treesitter-textobjects", lazy = false },
     -- Copilot
@@ -81,30 +75,37 @@ return {
       "zbirenbaum/copilot.lua",
       lazy = false,
       init = function()
-        require("copilot").setup({
+        require("copilot").setup {
           suggestion = {
             auto_trigger = true,
             keymap = {
               accept = "<C-l>",
-            }
-          }
-        })
-      end
+            },
+          },
+        }
+      end,
     },
     -- Automatically add closing brackets, etc.
-    { "windwp/nvim-autopairs",             lazy = false },
+    { "windwp/nvim-autopairs", lazy = false },
     -- Mason configuration
     {
       "williamboman/mason-lspconfig.nvim",
       lazy = false,
       opts = {
         ensure_installed = {
-          "lua_ls",
+          -- "lua_ls",
           "jsonls",
           "yamlls",
           "ruby_ls",
-        }
-      }
+          "sorbet",
+        },
+      },
+    },
+    {
+      "jay-babu/mason-null-ls.nvim",
+      opts = {
+        ensure_installed = { "jq", "stylua" },
+      },
     },
     {
       "nvim-treesitter/nvim-treesitter",
@@ -115,92 +116,117 @@ return {
     {
       "nvim-telescope/telescope.nvim",
       keys = {
-        { '<C-a>', "<cmd>Telescope telescope-alternate alternate_file<cr>" }
+        { "<C-a>", "<cmd>Telescope telescope-alternate alternate_file<cr>" },
       },
       opts = {
         pickers = {
           find_files = {
-            hidden = true
-          }
+            hidden = true,
+          },
         },
         extensions = {
           ["telescope-alternate"] = {
             presets = { "rails", "rspec" },
             mappings = {
-              { 'components/(.*)/app/.*/(.*).rb', {
-                { 'components/[1]/test/**/[2]_test.rb', 'Test' }
+              {
+                "components/(.*)/app/.*/(.*).rb",
+                {
+                  { "components/[1]/test/**/[2]_test.rb", "Test" },
+                },
+              },
+              {
+                "components/(.*)/test/.*/(.*)_test.rb",
+                {
+                  { "components/[1]/app/controllers/**/[2].rb", "Controller" },
+                  { "components/[1]/app/models/**/[2].rb", "Model" },
+                },
+              },
+              { "app/.*/(.*).rb", {
+                { "test/**/[1]_test.rb", "Test" },
               } },
-              { 'components/(.*)/test/.*/(.*)_test.rb', {
-                { 'components/[1]/app/controllers/**/[2].rb', 'Controller' },
-                { 'components/[1]/app/models/**/[2].rb',      'Model' },
+              { "test/.*/(.*)_test.rb", {
+                { "app/**/[1].rb", "Implementation" },
               } },
-              { 'app/.*/(.*).rb', {
-                { 'test/**/[1]_test.rb', 'Test' }
-              } },
-              { 'test/.*/(.*)_test.rb', {
-                { 'app/**/[1].rb', 'Implementation' }
-              } }
-            }
-          }
-        }
-      }
+              {
+                "gems/(.*)/lib/.*/(.*).rb",
+                {
+                  { "gems/[1]/test/**/[2]_test.rb", "Test" },
+                },
+              },
+              {
+                "gems/(.*)/test/.*/(.*)_test.rb",
+                {
+                  { "gems/[1]/**/[2].rb", "Implementation" },
+                },
+              },
+            },
+          },
+        },
+      },
     },
     {
       "vim-test/vim-test",
       keys = {
-        { "<leader>TT", vim.cmd.TestNearest, desc = "Test nearest" },
-        { "<leader>Tf", vim.cmd.TestFile,    desc = "Test file" },
-        { "<leader>Ts", vim.cmd.TestSuite,   desc = "Test suite" },
-        { "<leader>Tl", vim.cmd.TestLast,    desc = "Test last" },
-        { "<leader>Tv", vim.cmd.TestVisit,   desc = "Test visit" },
-      }
+        { "<leader>Tn", vim.cmd.TestNearest, desc = "Test nearest" },
+        { "<leader>Tf", vim.cmd.TestFile, desc = "Test file" },
+        { "<leader>Ts", vim.cmd.TestSuite, desc = "Test suite" },
+        { "<leader>TT", vim.cmd.TestLast, desc = "Test last" },
+        { "<leader>Tv", vim.cmd.TestVisit, desc = "Test visit" },
+      },
+      init = function()
+        vim.cmd [[
+          let test#strategy = "vimux"
+
+          let test#custom_runners = {'Generic': ['bintest'] }
+          if filereadable('bin/test')
+            let test#enabled_runners = ["generic#bintest"]
+          end
+        ]]
+      end,
     },
+    { "preservim/vimux", lazy = false },
     -- Configure cmp sources
     -- In particular, configure the buffer source to use all buffers
     {
       "hrsh7th/nvim-cmp",
       opts = function(_, opts)
-        local cmp = require("cmp")
-        local cmp_buffer = require("cmp_buffer")
+        local cmp = require "cmp"
+        local cmp_buffer = require "cmp_buffer"
         opts.sorting = {
           comparators = {
             -- Use cmp_buffer:compare_locality
-            function(...)
-              return cmp_buffer:compare_locality(...)
-            end,
-          }
+            function(...) return cmp_buffer:compare_locality(...) end,
+          },
         }
-        opts.sources = cmp.config.sources({
+        opts.sources = cmp.config.sources {
           { name = "nvim_lsp", priority = 1000 },
-          { name = "luasnip",  priority = 750 },
+          { name = "luasnip", priority = 750 },
           {
             name = "buffer",
             priority = 500,
             option = {
-              get_bufnrs = function()
-                return vim.api.nvim_list_bufs()
-              end,
+              get_bufnrs = function() return vim.api.nvim_list_bufs() end,
             },
           },
           { name = "path", priority = 250 },
-        })
+        }
         return opts
       end,
     },
     { "otavioschwanck/telescope-alternate" },
-    (vim.env.SPIN == '1' and { "Shopify/spin-hud" }) or nil,
+    (vim.env.SPIN == "1" and { "Shopify/spin-hud" }) or nil,
   },
   colorscheme = "dracula",
   mappings = {
     n = {
       ["<leader>E"] = { "<cmd>e ~/.config/nvim/lua/user/init.lua<cr>", desc = "Edit init.lua" },
-      ["n"]         = { "nzz" },
-      ["N"]         = { "Nzz" },
+      ["n"] = { "nzz" },
+      ["N"] = { "Nzz" },
     },
     t = {
       -- Ctrl-O in terminal to enter normal mode
       ["<C-O>"] = { "<C-\\><C-n>" },
-    }
+    },
   },
   options = {
     g = {
@@ -218,13 +244,19 @@ return {
   },
   lsp = {
     formatting = {
-      timeout_ms = 10000
+      timeout_ms = 10000,
     },
     config = {
+      sorbet = function(opts)
+        local util = require "lspconfig.util"
+        opts.root_dir = util.find_git_ancestor
+        -- vim.lsp.log("sorbet root_dir: " .. opts.root_dir)
+        return opts
+      end,
       ruby_ls = function(opts)
-        local util = require "lspconfig.util";
-        opts.cmd = { 'bundle', 'exec', 'ruby-lsp' }
-        opts.root_dir = util.find_git_ancestor()
+        local util = require "lspconfig.util"
+        opts.cmd = { "bundle", "exec", "ruby-lsp" }
+        opts.root_dir = util.find_git_ancestor
         opts.init_options = { formatting = "auto" }
         opts.on_attach = function(client, buffer)
           -- in the case you have an existing `on_attach` function
@@ -232,36 +264,29 @@ return {
           pcall(on_attach, client, buffer)
           local diagnostic_handler = function()
             local params = vim.lsp.util.make_text_document_params(buffer)
-            client.request(
-              'textDocument/diagnostic',
-              { textDocument = params },
-              function(err, result)
-                if err then
-                  local err_msg = string.format("ruby-lsp - diagnostics error - %s", vim.inspect(err))
-                  vim.lsp.log.error(err_msg)
-                end
-                if not result then return end
-                vim.lsp.diagnostic.on_publish_diagnostics(
-                  nil,
-                  vim.tbl_extend('keep', params, { diagnostics = result.items }),
-                  { client_id = client.id }
-                )
+            client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
+              if err then
+                local err_msg = string.format("ruby-lsp - diagnostics error - %s", vim.inspect(err))
+                vim.lsp.log.error(err_msg)
               end
-            )
+              if not result then return end
+              vim.lsp.diagnostic.on_publish_diagnostics(
+                nil,
+                vim.tbl_extend("keep", params, { diagnostics = result.items }),
+                { client_id = client.id }
+              )
+            end)
           end
           diagnostic_handler() -- to request diagnostics when attaching the client to the buffer
-          local ruby_group = vim.api.nvim_create_augroup('ruby_ls', { clear = false })
-          vim.api.nvim_create_autocmd(
-            { 'BufEnter', 'BufWritePre', 'InsertLeave', 'TextChanged' },
-            {
-              buffer = buffer,
-              callback = diagnostic_handler,
-              group = ruby_group,
-            }
-          )
+          local ruby_group = vim.api.nvim_create_augroup("ruby_ls", { clear = false })
+          vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePre", "InsertLeave", "TextChanged" }, {
+            buffer = buffer,
+            callback = diagnostic_handler,
+            group = ruby_group,
+          })
         end
         return opts
-      end
+      end,
     },
   },
 }
