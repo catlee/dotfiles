7ede092f55972e34f7c8692788398e8819f3bca6
Chris AtLee <chris.atlee@shopify.com>
2024-01-09 16:57:24 -0500

    use pull_diags plugin

--- vim/user_init.lua
+++ vim/user_init.lua
@@ -18,42 +18,6 @@ local function get_clipboard()
   end
 end
 
--- textDocument/diagnostic support until 0.10.0 is released
-local _timers = {}
-local function setup_diagnostics(client, buffer)
-  if require("vim.lsp.diagnostic")._enable then return end
-
-  local diagnostic_handler = function()
-    local params = vim.lsp.util.make_text_document_params(buffer)
-    client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
-      if err then
-        local err_msg = string.format("diagnostics error - %s", vim.inspect(err))
-        vim.lsp.log.error(err_msg)
-      end
-      local diagnostics = {}
-      if result then diagnostics = result.items end
-      vim.lsp.diagnostic.on_publish_diagnostics(
-        nil,
-        vim.tbl_extend("keep", params, { diagnostics = diagnostics }),
-        { client_id = client.id },
-        {}
-      )
-    end)
-  end
-
-  diagnostic_handler() -- to request diagnostics on buffer when first attaching
-
-  vim.api.nvim_buf_attach(buffer, false, {
-    on_lines = function()
-      if _timers[buffer] then vim.fn.timer_stop(_timers[buffer]) end
-      _timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
-    end,
-    on_detach = function()
-      if _timers[buffer] then vim.fn.timer_stop(_timers[buffer]) end
-    end,
-  })
-end
-
 return {
   updater = {
     channel = "stable",
@@ -335,6 +299,12 @@ return {
         },
       },
     },
+    {
+      "catlee/pull_diags.nvim",
+      -- dev = true,
+      event = "LspAttach",
+      opts = {},
+    },
     (vim.env.SPIN == "1" and { "Shopify/spin-hud" }) or nil,
   },
   colorscheme = "dracula",
@@ -375,7 +345,6 @@ return {
       end,
       ruby_ls = function(opts)
         opts.root_dir = require("lspconfig.util").find_git_ancestor
-        opts.on_attach = function(client, buffer) setup_diagnostics(client, buffer) end
         return opts
       end,
     },
