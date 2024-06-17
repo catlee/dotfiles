6319a3e221a78f89bb5d93e3379a3915f305a6b8
Chris AtLee <chris.atlee@shopify.com>
2024-06-17 09:36:22 -0400

    Bump

--- vim/astronvim/lua/community.lua
+++ vim/astronvim/lua/community.lua
@@ -2,5 +2,6 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript" },
   -- import/override with your plugins folder
 }
