9b9f428bd3e0fc7fbc865a35a8d9d67f174e6591
Chris AtLee <chris.atlee@shopify.com>
2024-09-04 13:40:45 -0400

    Trying out aerospace on mac

--- mac/hammerspoon_init.lua
+++ mac/hammerspoon_init.lua
@@ -2,42 +2,28 @@ hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
 
 hs.hotkey.bind({ "cmd", "shift" }, "return", function()
-    hs.execute("nohup kitty -d $HOME < /dev/null &> /dev/null &", true)
-end)
-
-hs.hotkey.bind({ "cmd", "shift" }, "h", function()
-    if hs.window.focusedWindow():focusWindowWest() then
-        local w = hs.window.focusedWindow()
-        hs.mouse.absolutePosition(w:frame().center)
-    end
-end)
-
-hs.hotkey.bind({ "cmd", "shift" }, "l", function()
-    if hs.window.focusedWindow():focusWindowEast() then
-        local w = hs.window.focusedWindow()
-        hs.mouse.absolutePosition(w:frame().center)
-    end
+	hs.execute("nohup kitty -d $HOME < /dev/null &> /dev/null &", true)
 end)
 
 --- Shift volume up/down for changing Spotify volume
 spotify_volume_handler = hs.eventtap
-    .new({ hs.eventtap.event.types.systemDefined }, function(event)
-        local currentMods = event:getFlags()
-        local data = event:systemKey()
+	.new({ hs.eventtap.event.types.systemDefined }, function(event)
+		local currentMods = event:getFlags()
+		local data = event:systemKey()
 
-        if data["key"] ~= "SOUND_UP" and data["key"] ~= "SOUND_DOWN" then
-            return false, nil
-        end
+		if data["key"] ~= "SOUND_UP" and data["key"] ~= "SOUND_DOWN" then
+			return false, nil
+		end
 
-        if not currentMods["shift"] then
-            return false, nil
-        end
+		if not currentMods["shift"] then
+			return false, nil
+		end
 
-        if data["key"] == "SOUND_UP" then
-            hs.spotify.setVolume(hs.spotify.getVolume() + 3)
-        else
-            hs.spotify.setVolume(hs.spotify.getVolume() - 3)
-        end
-        return true
-    end)
-    :start()
+		if data["key"] == "SOUND_UP" then
+			hs.spotify.setVolume(hs.spotify.getVolume() + 3)
+		else
+			hs.spotify.setVolume(hs.spotify.getVolume() - 3)
+		end
+		return true
+	end)
+	:start()
---
+++ vim/astronvim/lua/plugins/tmux-navigate.lua
@@ -0,0 +1,4 @@
+---@type LazySpec
+return {
+  { "sunaku/tmux-navigate", lazy = false },
+}
