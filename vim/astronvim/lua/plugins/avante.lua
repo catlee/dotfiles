dcf4f764cd5050c7a730193b2a2b761dc7a83489
Chris AtLee <chris.atlee@shopify.com>
2024-11-25 09:57:04 -0500

    latest

--- vim/astronvim/lua/plugins/avante.lua
+++ vim/astronvim/lua/plugins/avante.lua
@@ -3,7 +3,7 @@ return {
   "yetone/avante.nvim",
   event = "VeryLazy",
   lazy = false,
-  version = false,
+  -- version = false,
   config = function()
     local openai = require "avante.providers.openai"
     local has_openai_key = vim.fn.executable "openai_key" == 1
@@ -11,15 +11,17 @@ return {
     require("avante").setup {
       -- @type AvanteProvider
       provider = has_openai_key and "shopify-ai" or nil,
+      auto_suggestions_provider = has_openai_key and "shopify-ai" or nil,
       vendors = {
         ["shopify-ai"] = has_openai_key and {
           endpoint = "https://proxy.shopify.ai/v3/v1",
-          model = "anthropic:claude-3-5-sonnet",
+          model = "anthropic:claude-3-5-sonnet-v2",
           api_key_name = "cmd:openai_key cat",
           parse_curl_args = openai.parse_curl_args,
           parse_response_data = openai.parse_response,
         } or nil,
       },
+      hints = { enabled = true },
       highlights = {
         diff = {
           incoming = "DiffChange",
@@ -35,8 +37,8 @@ return {
   end,
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
   dependencies = {
+    "nvim-treesitter/nvim-treesitter",
     "stevearc/dressing.nvim",
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
